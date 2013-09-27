f13cf0b13f8429c21d4dca4dc02f07adaf8305bd
jmasters <redesitos@hotmail.com>
2013-09-26 20:26:58 -0500

    baner basico

--- Odontologia/application/Bootstrap.php
+++ Odontologia/application/Bootstrap.php
@@ -2,7 +2,16 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+ function _initViewHelpers() 
+    { 
+       $this->bootstrap('layout'); 
+       $layout = $this->getResource('layout'); 
+       $view = $layout->getView(); 
+       $view->doctype('XHTML1_STRICT'); 
+       $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8'); 
+       $view->headTitle()->setSeparator(' - '); 
+       $view->headTitle('Primeros pasos en Zend Framework'); 
+    }
 
 }
 
