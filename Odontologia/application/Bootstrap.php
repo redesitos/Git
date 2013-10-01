304723b6f42aae74ce72a8e926481ac382dcdc88
jmasters <redesitos@hotmail.com>
2013-09-30 21:59:50 -0500

    incluir contenido html en otro documento

--- Odontologia/application/Bootstrap.php
+++ Odontologia/application/Bootstrap.php
@@ -11,6 +11,17 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8'); 
        $view->headTitle()->setSeparator(' - '); 
        $view->headTitle('Primeros pasos en Zend Framework'); 
+      
+       $resourceAutoloader = new Zend_Loader_Autoloader_Resource(
+          array(
+            'basePath' => APPLICATION_PATH,
+            'namespace' => 'App',
+            'resourceTypes' => array(
+              'form' => array('path' => 'forms/', 'namespace' => 'Form'),
+              'model' => array('path' => 'models/', 'namespace' => 'Model')
+            )
+       )
+    );
     }
 
 }
