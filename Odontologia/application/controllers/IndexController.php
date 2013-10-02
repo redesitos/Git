336d1d4f1e71be5452e4a1271fe455fe7cd0454c
jmasters <redesitos@hotmail.com>
2013-10-01 22:29:38 -0500

    Conexion base de datos, model dbtable
    funcional

--- Odontologia/application/controllers/IndexController.php
+++ Odontologia/application/controllers/IndexController.php
@@ -1,5 +1,5 @@
 <?php
-
+use Application_Model_Register;
 class IndexController extends Zend_Controller_Action
 {
 
@@ -11,6 +11,25 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $_dbTable= new Application_Model_Register();
+        /*$_dbTable->createUser(array(
+                'id'=>2,
+                'name'=>'fish nano',
+                'age'=>10,
+                'gender'=>'M',
+                'company'=>0,
+                ));
+         */
+        /*$_dbTable->updateUser(array
+                (
+            'id'=>5,
+                'name'=>'fish2',
+                'age'=>50,
+                'gender'=>'F',
+                'company'=>10,
+                ), 2);
+        */
+        $_dbTable->deleteUser(1);
     }
 
 
