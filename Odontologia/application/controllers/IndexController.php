8c77709b1e67daf3e44a3ea54ac607a0b24e2672
jmasters <redesitos@hotmail.com>
2013-10-07 22:12:51 -0500

    vistas con el controlador
    pasar informacion del controlador a las vistas

--- Odontologia/application/controllers/IndexController.php
+++ Odontologia/application/controllers/IndexController.php
@@ -1,5 +1,6 @@
 <?php
 use Application_Model_Register;
+
 class IndexController extends Zend_Controller_Action
 {
 
@@ -11,7 +12,7 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-        $_dbTable= new Application_Model_Register();
+        //$_dbTable= new Application_Model_Register();
         /*$_dbTable->createUser(array(
                 'id'=>2,
                 'name'=>'fish nano',
@@ -29,9 +30,24 @@ class IndexController extends Zend_Controller_Action
                 'company'=>10,
                 ), 2);
         */
-        $_dbTable->deleteUser(1);
+        /*$_dbTable->deleteUser(1);*/
+        
+        /*$peticion = $this->getRequest();  
+        $valor = $peticion->getParam('variable');  */
+        
+       
+    }
+
+    public function addAction()
+    {
+        
+        $dato="fish";
+        $this->view->assign(array('dato'=>$dato));
+        return $this->view;
     }
 
 
 }
+
+
 
